07174ea96ffad7fe5cad1487f0b05f7b6aa2df17
stneajra <nerhan.ajradini@stud.h-da.de>
2023-12-05 18:25:17 +0100

    Changed something

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-
-</body>
-</html>
-
-
 <?php
+include 'parts/header.php';
+include 'parts/nav.php';
+echo <<< HTML
+<h1>test</h1>
+HTML;
+
+include 'parts/footer.php';
--- parts/nav.php
+++ parts/nav.php
@@ -1,1 +1,8 @@
 <?php
+
+echo <<< HTML
+<nav id="CMSNav">
+<a href="login.php">Login / Register</a>
+<a href="login.php">Dashboard</a>
+</nav>
+HTML;
